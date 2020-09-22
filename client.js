b8f36ba22da05d714d8a6311fc0594fe959861f9
Danielle Bentivoglio Colturato <dbcolturato@gmail.com>
2020-09-22 18:03:38 +0000

    Contants module

--- client.js
+++ client.js
@@ -1,12 +1,13 @@
 const net = require('net');
+const { IP, PORT } = require('./constants');
 
 /**
  * Establishes connection with the game server
  */
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '10.0.2.15',
-    port: 50541
+    host: IP,
+    port: PORT
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
@@ -14,13 +15,6 @@ const connect = function() {
     console.log('Successfully connected to game server');
     conn.write("Name: DBC");
   });
-  // conn.on('connect', () => {
-  //   conn.write("Move: down");
-  //   setTimeout(() => conn.write("Move: left"), 1000);
-  //   setTimeout(() => conn.write("Move: left"), 2000);
-  //   let interval = setInterval(() => conn.write("Move: left"), 50);
-  //   clearInterval(interval);
-  // });
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
